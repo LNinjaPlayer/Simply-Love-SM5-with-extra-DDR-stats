4f1c03cb69db92d0314b1976424de21bb79ad934
LNinjaPlayer <51856311+LNinjaPlayer@users.noreply.github.com>
2026-06-15 17:38:24 +0200

    Add nil check for current song in jacket.lua
    wrong check from previous commit

--- BGAnimations/ScreenSelectMusic overlay/jacket.lua
+++ BGAnimations/ScreenSelectMusic overlay/jacket.lua
@@ -1,5 +1,4 @@
 local SongOrCourse = GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
-if not SongOrCourse then return end
 
 local t = Def.ActorFrame{
 	OnCommand=function(self)
@@ -16,6 +15,7 @@ t[#t+1] = Def.ActorFrame{
 		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end,
 		SetCommand=function(self)
 			local song = GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
+			if not song then return end
 			if song:HasJacket() then
 				self:visible(true)
 				local JacketPath = (song and song:GetJacketPath())
