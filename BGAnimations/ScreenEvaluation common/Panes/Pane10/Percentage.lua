3aa323467c90ca699e536e9ec10446ac2378ade7
LNinjaPlayer <51856311+LNinjaPlayer@users.noreply.github.com>
2026-06-24 16:09:43 +0200

    Update Percentage.lua

--- BGAnimations/ScreenEvaluation common/Panes/Pane10/Percentage.lua
+++ BGAnimations/ScreenEvaluation common/Panes/Pane10/Percentage.lua
@@ -41,7 +41,7 @@ local w1 = offsets[1]
 local w2 = offsets[2]
 local w3 = offsets[3]
 local w4 = offsets[4]
-local w5 = offsets[5]
+-- local w5 = offsets[5] -- unused here
 -- local Missed = offsets[6] -- unused here
 
 local MinesAvoided = (pss:GetRadarActual():GetValue("RadarCategory_Mines"))/4
